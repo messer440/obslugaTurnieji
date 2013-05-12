b473e5dce98ecaf19a2e553b008e94601d734ad1
messer440 <messer440@gmail.com>
2013-05-13 00:33:46 +0200

    nie dziala :)

--- mainWindow_ui.py
+++ mainWindow_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'mainWindow.ui'
 #
-# Created: Mon May 13 00:23:37 2013
+# Created: Mon May 13 00:32:36 2013
 #      by: PyQt4 UI code generator 4.10.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -27,37 +27,43 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(800, 600)
+        MainWindow.setMaximumSize(QtCore.QSize(1920, 600))
         self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setMinimumSize(QtCore.QSize(800, 0))
+        self.centralwidget.setMinimumSize(QtCore.QSize(800, 600))
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.gridLayout_2 = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setSizeConstraint(QtGui.QLayout.SetNoConstraint)
         self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
         self.gridLayout = QtGui.QGridLayout()
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.stackedWidget = QtGui.QStackedWidget(self.centralwidget)
+        self.stackedWidget.setMaximumSize(QtCore.QSize(1920, 1080))
         self.stackedWidget.setObjectName(_fromUtf8("stackedWidget"))
         self.page = QtGui.QWidget()
         self.page.setObjectName(_fromUtf8("page"))
+        self.verticalLayoutWidget_2 = QtGui.QWidget(self.page)
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(0, 0, 89, 558))
+        self.verticalLayoutWidget_2.setObjectName(_fromUtf8("verticalLayoutWidget_2"))
+        self.gridLayout_4 = QtGui.QGridLayout(self.verticalLayoutWidget_2)
+        self.gridLayout_4.setMargin(0)
+        self.gridLayout_4.setObjectName(_fromUtf8("gridLayout_4"))
+        self.pushButton = QtGui.QPushButton(self.verticalLayoutWidget_2)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.gridLayout_4.addWidget(self.pushButton, 0, 0, 1, 1)
         self.stackedWidget.addWidget(self.page)
         self.page_2 = QtGui.QWidget()
         self.page_2.setObjectName(_fromUtf8("page_2"))
         self.stackedWidget.addWidget(self.page_2)
-        self.gridLayout.addWidget(self.stackedWidget, 0, 2, 1, 1)
-        self.rightPanel = QtGui.QVBoxLayout()
-        self.rightPanel.setObjectName(_fromUtf8("rightPanel"))
+        self.gridLayout.addWidget(self.stackedWidget, 0, 1, 1, 1)
+        self.gridLayout_3 = QtGui.QGridLayout()
+        self.gridLayout_3.setObjectName(_fromUtf8("gridLayout_3"))
         self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
-        self.rightPanel.addWidget(self.pushButton_2)
+        self.gridLayout_3.addWidget(self.pushButton_2, 0, 0, 1, 1)
         self.pushButton_3 = QtGui.QPushButton(self.centralwidget)
         self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
-        self.rightPanel.addWidget(self.pushButton_3)
-        self.gridLayout.addLayout(self.rightPanel, 0, 6, 1, 1)
-        self.leftPanel = QtGui.QVBoxLayout()
-        self.leftPanel.setObjectName(_fromUtf8("leftPanel"))
-        self.pushButton = QtGui.QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.leftPanel.addWidget(self.pushButton)
-        self.gridLayout.addLayout(self.leftPanel, 0, 1, 1, 1)
+        self.gridLayout_3.addWidget(self.pushButton_3, 1, 0, 1, 1)
+        self.gridLayout.addLayout(self.gridLayout_3, 0, 5, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtGui.QStatusBar(MainWindow)
@@ -65,12 +71,12 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.stackedWidget.setCurrentIndex(1)
+        self.stackedWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "Obsługa Turnieji", None))
+        self.pushButton.setText(_translate("MainWindow", "PushButton", None))
         self.pushButton_2.setText(_translate("MainWindow", "Uczestnicy", None))
         self.pushButton_3.setText(_translate("MainWindow", "Korty", None))
-        self.pushButton.setText(_translate("MainWindow", "PushButton", None))
 
