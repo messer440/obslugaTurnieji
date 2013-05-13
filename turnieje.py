33810f2ad0ed5a4eed7d164c38be8438306c904d
messer440 <messer440@gmail.com>
2013-05-13 23:43:08 +0200

    struktura

--- turnieje.py
+++ turnieje.py
@@ -3,7 +3,7 @@
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtCore import SIGNAL, SLOT
 import sys, string, re
-import mainWindow_ui
+from src.ui import mainWindow_ui
 
 class Turnieje(QtGui.QMainWindow, mainWindow_ui.Ui_MainWindow):
 	def __init__(self, parent=None, name=None, fl=0):
