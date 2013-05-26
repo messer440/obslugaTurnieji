096581e044bf3178c8ffca146a4bd0e767d6e6c3
Live session user <lubuntu@lubuntu.(none)>
2013-05-27 01:24:06 +0200

    Ui match edit

---
+++ src/matchGUI.py
@@ -0,0 +1,33 @@
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import SIGNAL, SLOT
+import sys, string, re
+from src.ui import windowPlayers_ui
+import importGUI
+import player
+import myZODB, transaction
+
+
+class matchGUI(QtGui.QtForm, mecz_ui.Ui_Form):
+    def __init__(self,  posx,  posy,levelUp,  text_match=None,  parent=None, name=None):
+        super(matchGUI, self).__init__(parent)
+        self.setupUi(self)
+        self.otherWindow = None
+        self.initForm(self.playerIdx)
+        self.label.setText(self, player1.fName+" "+player1.lName)
+        self.label2.setText(self, player2.fName+" "+player2.lName)
+        self.noOfPlayers=0
+        ### SIGNALS ### #{{{
+        self.buttonModif.connect(self.self.pushButton, SIGNAL("clicked()"), self.submitMatch)
+    def submitMatch(self):
+        if self.spinBox.value()>self.spinBox2.value():
+            levelUp.addPlayer(this. player1)
+        else:
+            levelUp.addPlayer(this. player2)
+    def addPlayer(self, player):
+        if self.noOfPlayers==0:
+            self.label.setText(self, player.fName+" "+player.lName)
+            self.player1=player
+        elif  self.noOfPlayers==1:
+            self.label2.setText(self, player.fName+" "+player.lName)
+            self.player2=player
+        self.noOfPlayers=self.noOfPlayers+1
